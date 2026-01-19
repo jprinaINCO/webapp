62d781f12ab1037bb194a3cbc89d07422738878c
jprinaINCO <jprina@incosoluciones.com>
2026-01-19 12:18:31 -0300

    Commit user1

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola desde EKS 18"
+    return "Hola desde EKS 0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
